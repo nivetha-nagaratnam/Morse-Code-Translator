5cd14922f199ebe7eaa7d03ee619c903322fa6c0
nivetha-nagaratnam <71537788+nivetha-nagaratnam@users.noreply.github.com>
2020-10-01 11:22:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+const converter = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    "'": '.----.',
+    '!': '-.-.--',
+    '/': '-..-.',
+    '(': '-.--.',
+    ')': '-.--.-',
+    '&': '.-...',
+    ':': '---...',
+    ';': '-.-.-.',
+    '=': '-...-',
+    '+': '.-.-.',
+    '-': '-....-',
+    '_': '..--.-',
+    '"': '.-..-.',
+    '$': '...-..-',
+    '@': '.--.-.',
+    ' ': ' '
+  }
+
+  const input = document.getElementById('myinput');
+  const span = document.querySelector('#myspan')
+  const button = document.querySelector('button')
+
+  button.addEventListener('click',function(){
+    console.log("hello");
+    let text = input.value;
+    console.log(text)
+    let splitText = text.split('');
+    console.log(splitText)
+    let translation = '';
+    splitText.forEach(function(i){
+      translation += converter[i]
+      console.log(converter[i])
+      console.log(translation)
+    });
+    myspan.textContent = translation;
+  })
